2e0f3a1c1a72b16f42056a39f3bac2677bafdf8c
Byasar3 <bernayasar@hotmail.co.uk>
2024-05-24 14:12:40 +0100

    commented code

--- src/main/java/PapazKimde/Computer.java
+++ src/main/java/PapazKimde/Computer.java
@@ -1,17 +1,17 @@
-package PapazKimde;
-
-public class Computer extends Player{
-
-    // override methods for automated behavior
-    @Override
-    public void removePairs() {
-        // automatically remove pairs for computer player
-    }
-
-    @Override
-    public void pullCardFrom(Player otherPlayer) {
-        // automatically pull a card from another player's hand for computer player
-    }
-
-    // more methods specific to computer players
-}
+//package PapazKimde;
+//
+//public class Computer extends Player{
+//
+//    // override methods for automated behavior
+//    @Override
+//    public void removePairs() {
+//        // automatically remove pairs for computer player
+//    }
+//
+//    @Override
+//    public void pullCardFrom(Player otherPlayer) {
+//        // automatically pull a card from another player's hand for computer player
+//    }
+//
+//    // more methods specific to computer players
+//}
