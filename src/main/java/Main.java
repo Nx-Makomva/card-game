afd63392cc2f6ef1e1d4e3b225fbdb16806347cc
Nic Makomva <nicholasstm@gmail.com>
2024-05-29 14:15:07 +0100

    cleaned main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,6 @@ public class Main {
     public static void main(String[] args) {
         PapazKimde whoHasTheKing = new PapazKimde();
         whoHasTheKing.playPapazKimde();
-//        Deck deck = new Deck();
-//        deck.printDeck();
         BlackJackMain.startGame();
     }
 }
